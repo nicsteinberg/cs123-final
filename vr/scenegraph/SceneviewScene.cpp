fe33ff0dc050853b6825daf918c1c9b64a96e2dd
mattlsteinberg <bli0428>
2018-12-09 17:15:28 -0500

    further DoF shader attempts

--- vr/scenegraph/SceneviewScene.cpp
+++ vr/scenegraph/SceneviewScene.cpp
@@ -29,8 +29,8 @@ SceneviewScene::SceneviewScene(int width, int height)
     // Is this the correct width and height? Takes in number of color attachments - we'll have at least 3.
     // Can also just make our own gBuffer class instead of an FBO that generates our gPosition, gNormal, and gColor buffers.
     m_FBO = std::make_unique<FBO>(3, FBO::DEPTH_STENCIL_ATTACHMENT::DEPTH_ONLY, width, height, TextureParameters::WRAP_METHOD::CLAMP_TO_EDGE, TextureParameters::FILTER_METHOD::LINEAR, GL_FLOAT);
-//    m_blurFBO1 = std::make_unique<FBO>(3, FBO::DEPTH_STENCIL_ATTACHMENT::DEPTH_ONLY, width, height, TextureParameters::WRAP_METHOD::CLAMP_TO_EDGE, GL_FLOAT);
-//    m_blurFBO2 = std::make_unique<FBO>(3, FBO::DEPTH_STENCIL_ATTACHMENT::DEPTH_ONLY, width, height, TextureParameters::WRAP_METHOD::CLAMP_TO_EDGE, GL_FLOAT);
+    m_blurFBO1 = std::make_unique<FBO>(3, FBO::DEPTH_STENCIL_ATTACHMENT::DEPTH_ONLY, width, height, TextureParameters::WRAP_METHOD::CLAMP_TO_EDGE, TextureParameters::FILTER_METHOD::LINEAR, GL_FLOAT);
+    m_blurFBO2 = std::make_unique<FBO>(3, FBO::DEPTH_STENCIL_ATTACHMENT::DEPTH_ONLY, width, height, TextureParameters::WRAP_METHOD::CLAMP_TO_EDGE, TextureParameters::FILTER_METHOD::LINEAR, GL_FLOAT);
 
     // Initialize shape member variables.
     settingsChanged();
@@ -48,30 +48,28 @@ void SceneviewScene::loadGeometryShader() {
 
 void SceneviewScene::loadPhongShader() {
     std::string vertexSource = ResourceLoader::loadResourceFileToString(":/shaders/shader.vert");
-        std::string fragmentSource = ResourceLoader::loadResourceFileToString(":/shaders/shader.frag");
-        // std::string vertexSource = ResourceLoader::loadResourceFileToString(":/shaders/quad.vert");
-        // std::string fragmentSource = ResourceLoader::loadResourceFileToString(":/shaders/lightshader.frag");
-        m_phongShader = std::make_unique<CS123Shader>(vertexSource, fragmentSource);
-        m_phongProgram = ResourceLoader::createShaderProgram(":/shaders/shader.vert", ":/shaders/shader.frag");
-        m_blurHProgram = ResourceLoader::createShaderProgram(":/shaders/quad.vert", ":/shaders/horizontalBlur.frag");
-        m_blurVProgram = ResourceLoader::createShaderProgram(":/shaders/quad.vert", ":/shaders/verticalBlur.frag");
+    std::string fragmentSource = ResourceLoader::loadResourceFileToString(":/shaders/shader.frag");
+    // std::string vertexSource = ResourceLoader::loadResourceFileToString(":/shaders/quad.vert");
+    // std::string fragmentSource = ResourceLoader::loadResourceFileToString(":/shaders/lightshader.frag");
+    m_phongShader = std::make_unique<CS123Shader>(vertexSource, fragmentSource);
+    m_phongProgram = ResourceLoader::createShaderProgram(":/shaders/shader.vert", ":/shaders/shader.frag");
+    m_blurHProgram = ResourceLoader::createShaderProgram(":/shaders/quad.vert", ":/shaders/horizontalBlur.frag");
+    m_blurVProgram = ResourceLoader::createShaderProgram(":/shaders/quad.vert", ":/shaders/verticalBlur.frag");
 
-        std::vector<GLfloat> quadData = {-1.f, -1.f, 0.f,
-                                         0, 1,
-                                         -1.f, +1.f, 0.f,
-                                         0, 0,
-                                         +1.f, -1.f, 0.f,
-                                         1, 1,
-                                         +1.f, +1.f, 0.f,
-                                         1, 0};
-        m_quad = std::make_unique<OpenGLShape>();
-        m_quad->setVertexData(&quadData[0], quadData.size(), VBO::GEOMETRY_LAYOUT::LAYOUT_TRIANGLE_STRIP, 4);
-        m_quad->setAttribute(ShaderAttrib::POSITION, 3, 0, VBOAttribMarker::DATA_TYPE::FLOAT, false);
-        m_quad->setAttribute(ShaderAttrib::TEXCOORD0, 2, 3*sizeof(GLfloat), VBOAttribMarker::DATA_TYPE::FLOAT, false);
-        m_quad->buildVAO();
+    std::vector<GLfloat> quadData = {-1.f, -1.f, 0.f,
+                                     0, 1,
+                                     -1.f, +1.f, 0.f,
+                                     0, 0,
+                                     +1.f, -1.f, 0.f,
+                                     1, 1,
+                                     +1.f, +1.f, 0.f,
+                                     1, 0};
+    m_quad = std::make_unique<OpenGLShape>();
+    m_quad->setVertexData(&quadData[0], quadData.size(), VBO::GEOMETRY_LAYOUT::LAYOUT_TRIANGLE_STRIP, 4);
+    m_quad->setAttribute(ShaderAttrib::POSITION, 3, 0, VBOAttribMarker::DATA_TYPE::FLOAT, false);
+    m_quad->setAttribute(ShaderAttrib::TEXCOORD0, 2, 3*sizeof(GLfloat), VBOAttribMarker::DATA_TYPE::FLOAT, false);
+    m_quad->buildVAO();
 
-        m_blurFBO1 = std::make_unique<FBO>();
-        m_blurFBO2 = std::make_unique<FBO>();
 }
 
 //void SceneviewScene::render(glm::mat4x4 projectionMatrix, glm::mat4x4 viewMatrix) {
@@ -125,41 +123,96 @@ void SceneviewScene::loadPhongShader() {
 //}
 
 // THIS IS THE WORKING ONE
-//void SceneviewScene::render(glm::mat4x4 projectionMatrix, glm::mat4x4 viewMatrix) {
-//    m_phongShader->bind();
-//    setSceneUniforms(projectionMatrix, viewMatrix);
-//    setLights();
-
-//    // Set "m" uniform and ambient, diffuse, specular, etc uniforms.
-//    renderGeometry();
-
-//    glBindTexture(GL_TEXTURE_2D, 0);
-//    m_phongShader->unbind();
-
-//}
-
 void SceneviewScene::render(glm::mat4x4 projectionMatrix, glm::mat4x4 viewMatrix) {
-
+    m_phongShader->bind();
     setSceneUniforms(projectionMatrix, viewMatrix);
     setLights();
 
-    m_blurFBO1->bind();
-    glUseProgram(m_phongProgram);
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClear(GL_DEPTH_BUFFER_BIT);
-
-    // neglected glViewport(0, 0, width, height) - add later?
+    // Set "m" uniform and ambient, diffuse, specular, etc uniforms.
     renderGeometry();
-    m_blurFBO1->unbind();
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    m_phongShader->unbind();
 
 }
 
+// matt's attempts at depth of field
+//void SceneviewScene::render(glm::mat4x4 projectionMatrix, glm::mat4x4 viewMatrix) {
+
+//    // BEGIN matt tried a thing here
+//    //    glUniformMatrix4fv(glGetUniformLocation(m_phongProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+//    //    glUniformMatrix4fv(glGetUniformLocation(m_phongProgram, "view"), 1, GL_FALSE, glm::value_ptr(viewMatrix));
+//    //    setLights();
+
+//    //    m_blurFBO1->bind();
+//    //    glUseProgram(m_phongProgram);
+
+//    //    glClear(GL_COLOR_BUFFER_BIT);
+//    //    glClear(GL_DEPTH_BUFFER_BIT);
+
+//    //    // neglected glViewport(0, 0, width, height) - add later?
+//    //    renderGeometry();
+//    //    m_blurFBO1->unbind();
+//    // END matt tried a thing here
+
+//    //    // Bind FBO.
+//    m_blurFBO1->bind();
+
+//    //    // Use this program ID.
+//    //    m_geoShader->bind();
+//    m_phongShader->bind();
+
+//    //    // Is this necessary? Why these bits?
+//    //    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//    glClear(GL_COLOR_BUFFER_BIT);
+//    glClear(GL_DEPTH_BUFFER_BIT);
+
+//    //    // Is this necessary?
+//    //    glViewPort(0, 0, width, height);
+
+//    //    // Set "m" uniform and ambient, diffuse, specular, etc uniforms.
+//    renderGeometry();
+
+//    //    // Gives texture to .frag? Which texture is this?
+//    glBindTexture(GL_TEXTURE_2D, 0);
+
+//    //    // Unbind geometry shader.
+//    //    m_geoShader->unbind();
+//    m_phongShader->unbind();
+
+//    //    // Should we call this before or after unbinding geoshader?
+//    m_blurFBO1->unbind();
+
+//    //    // bind all our textures (gPosition, gNormal, gColor)
+//    //    // are activeTexture calls necessary?
+//    glActiveTexture(GL_TEXTURE0);
+//    m_FBO->getColorAttachment(0).bind();
+//    glActiveTexture(GL_TEXTURE1);
+//    m_FBO->getColorAttachment(1).bind();
+//    glActiveTexture(GL_TEXTURE2);
+//    m_FBO->getColorAttachment(2).bind();
+
+//    //    // Bind light shader.
+//    m_phongShader->bind();
+
+//    //    // Set uniforms passed into vert.
+//    setSceneUniforms(projectionMatrix, viewMatrix);
+
+//    //    // Send lighting uniforms to fragment shader.
+//    setLights();
+
+//    m_phongShader->unbind();
+
+//    //    // render quad
+//    m_quad->draw();
+
+//}
+
 void SceneviewScene::render(
-    glm::mat4x4 projectionMatrix,
-    glm::mat4x4 viewMatrix,
-    glm::mat4 m_mat4DevicePose[vr::k_unMaxTrackedDeviceCount],
-    bool m_activeTrackedDevice[vr::k_unMaxTrackedDeviceCount]) {
+        glm::mat4x4 projectionMatrix,
+        glm::mat4x4 viewMatrix,
+        glm::mat4 m_mat4DevicePose[vr::k_unMaxTrackedDeviceCount],
+bool m_activeTrackedDevice[vr::k_unMaxTrackedDeviceCount]) {
 
     // TODO: use controller positions if necessary
     render(projectionMatrix, viewMatrix);
@@ -167,14 +220,10 @@ void SceneviewScene::render(
 
 
 void SceneviewScene::setSceneUniforms(glm::mat4x4 &projectionMatrix, glm::mat4x4 &viewMatrix) {
-//     WE WANT THESE 4 LINES BACK
-//    m_phongShader->setUniform("useLighting", true);
-//    m_phongShader->setUniform("useArrowOffsets", false);
-//    m_phongShader->setUniform("p", projectionMatrix);
-//    m_phongShader->setUniform("v", viewMatrix);
-
-    glUniformMatrix4fv(glGetUniformLocation(m_phongProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projectionMatrix));
-    glUniformMatrix4fv(glGetUniformLocation(m_phongProgram, "view"), 1, GL_FALSE, glm::value_ptr(viewMatrix));
+    m_phongShader->setUniform("useLighting", true);
+    m_phongShader->setUniform("useArrowOffsets", false);
+    m_phongShader->setUniform("p", projectionMatrix);
+    m_phongShader->setUniform("v", viewMatrix);
 
     //m_geoShader->setUniform("p", projectionMatrix);
     //m_geoShader->setUniform("v", viewMatrix);
@@ -202,22 +251,22 @@ void SceneviewScene::renderGeometry() {
         //m_geoShader->setUniform("m", prim.compositeTransformation);
         //m_geoShader->applyMaterial(prim.material); // Will have to add more uniforms to this helper!
 
-//        if (prim.material.textureMap.isUsed) {
-//            getTexture(prim.material);
-//        }
+        //        if (prim.material.textureMap.isUsed) {
+        //            getTexture(prim.material);
+        //        }
 
         switch (prim.type) {
-            case PrimitiveType::PRIMITIVE_CUBE:
-                m_cube->draw();
-                break;
-            case PrimitiveType::PRIMITIVE_CONE:
-                m_cone->draw();
-                break;
-            case PrimitiveType::PRIMITIVE_CYLINDER:
-                m_cylinder->draw();
-                break;
-            default:
-                m_sphere->draw();
+        case PrimitiveType::PRIMITIVE_CUBE:
+            m_cube->draw();
+            break;
+        case PrimitiveType::PRIMITIVE_CONE:
+            m_cone->draw();
+            break;
+        case PrimitiveType::PRIMITIVE_CYLINDER:
+            m_cylinder->draw();
+            break;
+        default:
+            m_sphere->draw();
         }
     }
 }
